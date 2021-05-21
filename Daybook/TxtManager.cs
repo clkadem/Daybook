0f77efb658c7c4a2ad92222914c5bf11010eb8a9
clkadem <47740206+clkadem@users.noreply.github.com>
2021-05-21 21:21:04 +0300

    Add files via upload

---
+++ Daybook/TxtManager.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class TxtManager
+    {
+        public void Yazma(int test)
+        {
+            string dosyaYolu = "C:\\Users\\Adem\\source\\repos\\ConsoleApp1\\ConsoleApp1\\UID.txt";
+            File.WriteAllText(dosyaYolu, string.Empty);
+            FileStream fileStream = new FileStream(dosyaYolu, FileMode.Open, FileAccess.Write);
+            StreamWriter writer = new StreamWriter(fileStream, Encoding.UTF8);
+            writer.WriteLine(test);
+            writer.Close();
+        }
+
+        public int Okuma()
+        {
+            string dosyaYolu = "C:\\Users\\Adem\\source\\repos\\ConsoleApp1\\ConsoleApp1\\UID.txt";
+            FileStream fileStream = new FileStream(dosyaYolu, FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamReader reader = new StreamReader(fileStream);
+            
+            
+            string satir = reader.ReadLine();
+
+            reader.Close();
+
+            return int.Parse(satir);
+        }
+    }
+}
+    
