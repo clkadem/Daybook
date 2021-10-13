d6fd30ef4adf4b1bcf1338a7dc823d43ead6c29c
clkadem <47740206+clkadem@users.noreply.github.com>
2021-10-13 12:46:20 +0300

    Update AwsConnection.cs

--- Daybook/AwsConnection.cs
+++ Daybook/AwsConnection.cs
@@ -8,7 +8,7 @@ namespace Daybook
 {
     public class AwsConnection
     {
-        public MySqlConnection sqlConnection = new MySqlConnection("Server=denek-1.cua7nr2cnq3x.us-east-2.rds.amazonaws.com;Database=daybook;Uid=masterUserAdem;Pwd=Adem3012;");
+        public MySqlConnection sqlConnection = new MySqlConnection("Server=denek-1.cua7nr2cnq3x.us-east-2.rds.amazonaws.com;Database=daybook;Uid=masterUserAdem;Pwd=********;");
     }
 }
     
